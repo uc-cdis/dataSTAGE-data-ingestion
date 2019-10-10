6773fc26ff18604cad1362db2566d337142ee696
Zakir Gowani <zakirgowani@gmail.com>
2019-10-10 12:38:19 -0500

    added automated PR-making step

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ set -o pipefail
 
 ###############################################################################
 # 1. Create a manifest from a bucket
-echo "creds_json env var: "
 echo $CREDS_JSON
 export AWS_ACCESS_KEY_ID=$(jq -r .aws_creds.aws_access_key_id <<< $CREDS_JSON)
 export AWS_SECRET_ACCESS_KEY=$(jq -r .aws_creds.aws_secret_access_key <<< $CREDS_JSON)
@@ -17,7 +16,7 @@ export GCP_PROJECT_ID=$(jq -r .gcp_project_id <<< $CREDS_JSON)
 export GS_BUCKET_PATH=$(jq -r .gs_bucket_path <<< $CREDS_JSON)
 export AWS_BUCKET_PATH=$(jq -r .aws_bucket_path <<< $CREDS_JSON)
 
-aws s3 ls
+# aws s3 ls
 
 cd scripts
 echo $GS_CREDS_JSON >> gs_cloud_key.json
@@ -27,19 +26,21 @@ echo $GS_CREDS_JSON >> gs_cloud_key.json
 
 ###############################################################################
 # 2. Create extract file
+cd /
 git clone https://github.com/uc-cdis/dbgap-extract.git
 cd dbgap-extract
 # TODO: alter these 2 lines to use master branch
 git checkout feat/validate-extract
 git pull origin feat/validate-extract
 pipenv install
-pipenv run python3 dbgap_extract.py --study_accession_list_filename ../test_phs_list.txt --output_filename generated_extract.tsv
-
+pipenv run python3 dbgap_extract.py --study_accession_list_filename /dataSTAGE-data-ingestion/test_phs_list.txt --output_filename generated_extract.tsv
+ls
+pwd
 
 ###############################################################################
 # 3. Generate a list of commands to create Google Groups and a mapping file for the joindure script
 cd /dataSTAGE-data-ingestion/scripts/
-python3 generate_google_group_cmds.py --extract_filename ../dbgap-extract/generated_extract.tsv
+python3 generate_google_group_cmds.py --extract_filename /dbgap-extract/generated_extract.tsv
 if [ -f "scripts/google-groups.sh" ]; then
   chmod +x scripts/google-groups.sh
 fi
@@ -51,6 +52,25 @@ mv mapping.txt /dataSTAGE-data-ingestion/scripts/joindure/mapping.txt
 cd /dataSTAGE-data-ingestion/scripts/joindure
 mkdir output
 pipenv run python3 main.py merge --genome_manifest /dataSTAGE-data-ingestion/genome_file_manifest.csv \
-    --dbgap_manifest /dataSTAGE-data-ingestion/dbgap-extract/generated_extract.tsv --out output
+    --dbgap_manifest /dbgap-extract/generated_extract.tsv --out output
 
 ls output
+
+
+###############################################################################
+# 5. Make PR to repo with outputs
+cd /
+git clone http://github.com/uc-cdis/dataSTAGE-data-ingestion-private.git
+cd dataSTAGE-data-ingestion-private/
+git pull origin master && git fetch --all
+BRANCH_NAME=$(python3 /dataSTAGE-data-ingestion/scripts/get_branch_name.py --current_branches "$(git branch -a)")
+echo "Creating branch ${BRANCH_NAME}"
+git checkout -b $BRANCH_NAME
+cp -R /dataSTAGE-data-ingestion/scripts/joindure/output/. .
+cp /dbgap-extract/generated_extract.tsv .
+# cp /dbgap-extract/generated_extract.log .
+
+git status
+
+# TODO: uncomment this line
+# git push origin $BRANCH_NAME
---
+++ scripts/get_branch_name.py
@@ -0,0 +1,42 @@
+import argparse
+import sys
+import subprocess
+import logging
+from datetime import datetime
+
+BRANCH_NAME_PREFIX = 'feat/release-'
+
+def get_branch_name(git_branch_a):
+	branches = git_branch_a.split()
+	branches = list(filter(lambda x: BRANCH_NAME_PREFIX in x, branches))
+	releases = list(map(lambda x: int(x.strip()[-1]), branches))
+	if len(releases) == 0:
+		return BRANCH_NAME_PREFIX + str(1)
+	else:
+		return BRANCH_NAME_PREFIX + str(max(releases) + 1)
+
+def main():
+    parser = argparse.ArgumentParser(description="Determine new branch name for release manifest output.")
+    parser.add_argument(
+        "--current_branches",
+        help="the output of git branch -a",
+    )
+
+    args = parser.parse_args(sys.argv[1:])
+
+    if not args.current_branches:
+        logging.debug("-------")
+        logging.debug(
+            "Usage error. Run this script using the below command form:"
+        )
+        logging.debug(
+            "> python get_branch_name.py --current_branches <the output of git branch -a>"
+        )
+        logging.debug("-------")
+        exit(0)
+   
+    branch_name = get_branch_name(args.current_branches)
+    print(branch_name)
+
+if __name__ == '__main__':
+	main()
