d09b66687ee820089f9b4c6446b0781f7897eeec
Alexander VanTol <Avantol13@users.noreply.github.com>
2020-03-23 17:12:04 -0500

    Update test_all.sh

--- scripts/tests/test_all.sh
+++ scripts/tests/test_all.sh
@@ -5,7 +5,7 @@ sudo ./install.sh /usr/local
 cd ..
 bats *.bats
 
-pip install --user pipenv
+curl https://raw.githubusercontent.com/kennethreitz/pipenv/master/get-pipenv.py | python || pip install pipenv
 pipenv install --dev
 pipenv run python -m pytest tests.py
 
