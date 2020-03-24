f92bf20a457a2b8b0498867dd61a9ced2d5b4510
Zakir Gowani <zakirgowani@gmail.com>
2020-03-24 12:04:02 -0500

    fix: bats install was failing on mac

--- scripts/tests/test_all.sh
+++ scripts/tests/test_all.sh
@@ -1,12 +1,35 @@
-git clone https://github.com/bats-core/bats-core.git
-cd bats-core
-sudo ./install.sh /usr/local
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo "Running on ${machine}"
+
+bats -v
+if [ $? != 0 ] && [ $machine != 'Mac' ]; then
+    git clone https://github.com/bats-core/bats-core.git
+    cd bats-core
+    sudo ./install.sh /usr/local
+    cd ..
+fi
+
+bats -v
+if [ $? != 0 ] && [ $machine == 'Mac' ]; then
+    brew install bats-core
+fi
 
 cd ..
-bats *.bats
+bats tests/*.bats
 
-curl https://raw.githubusercontent.com/kennethreitz/pipenv/master/get-pipenv.py | python || pip install pipenv
+pipenv --version
+if [ $? != 0 ]; then
+    curl https://raw.githubusercontent.com/kennethreitz/pipenv/master/get-pipenv.py | python || pip install pipenv
+fi
 pipenv install --dev
+cd tests
 pipenv run python -m pytest tests.py
 
 exit
